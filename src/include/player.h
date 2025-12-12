22e34960db919c381bdf4e7e03cd483d4108ecd9
Frode Ulriksen <frode.ulriksen@outlook.com>
2025-12-12 19:35:38 +0100

    .

--- src/include/player.h
+++ src/include/player.h
@@ -8,6 +8,6 @@ typedef struct Player {
 } Player;
 
 void PlayerUpdatePosition(Player *player, bool TwoPlayer);
-void TwoPlayerCollision(Player *player1, Player *player2);
 
 void PlayerCheckCollision(Player *player, int screenWidth, int screenHeight);
+void TwoPlayerCollision(Player *player1, Player *player2);
